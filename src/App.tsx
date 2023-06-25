3788aa495be09eade487558056960378a6271290
gio797 <119562363+gio797@users.noreply.github.com>
2023-06-25 13:27:40 +0400

    add styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import iconDollar from "../src/images/icon-dollar.svg";
+import iconPerson from "../src/images/icon-person.svg";
 
 function App() {
   const [bill, setBill] = useState<number | null>(null);
@@ -25,53 +27,116 @@ function App() {
     }
   }, [people]);
 
+  function handleReset() {
+    setBill(null);
+    setPeople(null);
+  }
+
   return (
-    <div className="">
-      bill:
-      <input
-        type="number"
-        placeholder="bill"
-        name="bill"
-        value={bill !== null ? bill.toString() : ""}
-        onChange={(e) => setBill(parseFloat(e.target.value))}
-      />
-      <div className="buttons">
-        <button onClick={() => setTip(0.05)}>5%</button>
+    <div className="site -wrapper">
+      <h1 className="title-h1">
+        <span>spli</span>
+        <br />
+        <span>tter</span>
+      </h1>
+      <div className="app">
+        <div className="inputs">
+          <label htmlFor="bill">Bill</label>
+          <div className="input-icons">
+            <div className="icon">
+              <img src={iconDollar} alt="" />
+            </div>
+            <input
+              className="input-field"
+              type="number"
+              id="bill"
+              value={bill !== null ? bill.toString() : ""}
+              onChange={(e) => setBill(parseFloat(e.target.value))}
+            />
+          </div>
+          <h4>Select Tip %</h4>
+          <div className="buttons">
+            <button className="tip-Perc-btn" onClick={() => setTip(0.05)}>
+              5%
+            </button>
 
-        <button onClick={() => setTip(0.1)}>10%</button>
+            <button className="tip-Perc-btn" onClick={() => setTip(0.1)}>
+              10%
+            </button>
 
-        <button onClick={() => setTip(0.15)}>15%</button>
+            <button className="tip-Perc-btn" onClick={() => setTip(0.15)}>
+              15%
+            </button>
 
-        <button onClick={() => setTip(0.25)}>25%</button>
+            <button className="tip-Perc-btn" onClick={() => setTip(0.25)}>
+              25%
+            </button>
 
-        <button onClick={() => setTip(0.5)}>50%</button>
-        <input
-          type="number"
-          min={0}
-          max={100}
-          placeholder="custom"
-          name="custom"
-          value={tip !== null ? (tip * 100).toString() : ""}
-          onChange={(e) => setTip(parseFloat(e.target.value) / 100)}
-        />
+            <button className="tip-Perc-btn" onClick={() => setTip(0.5)}>
+              50%
+            </button>
+            <input
+              className="custom-inp"
+              type="number"
+              min={0}
+              max={100}
+              placeholder="Custom"
+              id="custom"
+              value={tip !== null ? (tip * 100).toString() : ""}
+              onChange={(e) => setTip(parseFloat(e.target.value) / 100)}
+            />
+          </div>
+          <label htmlFor="people">Number Of People:</label>
+          <div className="input-icons">
+            <div className="icon">
+              <img src={iconPerson} alt="" />
+            </div>
+            <input
+              className="input-field"
+              min={0}
+              type="number"
+              value={people !== null ? people.toString() : ""}
+              id="people"
+              onKeyDown={(e) => {
+                if (e.key === ".") {
+                  e.preventDefault();
+                }
+              }}
+              onChange={(e) =>
+                setPeople(Math.trunc(parseFloat(e.target.value)))
+              }
+            />
+          </div>
+          <div>{peopleError ? "Can't be zero" : ""}</div>
+        </div>
+        <div className="result-display">
+          <div className="dipslays">
+            <div className="tip-displ">
+              <p>
+                Tip amount
+                <br />
+                <span className="tip-displ-person">/ person</span>
+              </p>
+              <p className="tip-displ-amount">
+                ${showTip ? tipAmount : "0.00"}
+              </p>
+            </div>
+            <div className="tip-displ">
+              <p>
+                Total
+                <br />
+                <span className="tip-displ-person">/ person</span>
+              </p>
+              <p className="tip-displ-amount">
+                ${showTotal ? totalPerPerson : "0.00"}
+              </p>
+            </div>
+          </div>
+          <button className="reset-btn" onClick={handleReset}>
+            reset
+          </button>
+        </div>
       </div>
-      People:
-      <input
-        min={0}
-        type="number"
-        value={people !== null ? people.toString() : ""}
-        name="people"
-        placeholder="number of people"
-        onKeyDown={(e) => {
-          if (e.key === ".") {
-            e.preventDefault();
-          }
-        }}
-        onChange={(e) => setPeople(Math.trunc(parseFloat(e.target.value)))}
-      />
-      <div>{peopleError ? "Can't be zero" : ""}</div>
-      <div>Tip amount / person ${showTip ? tipAmount : "0.00"}</div>
-      <div>Total / person ${showTotal ? totalPerPerson : "0.00"}</div>
     </div>
   );
 }
